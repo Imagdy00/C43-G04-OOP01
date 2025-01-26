bcb02db83ae2a02a95a83f6676f63dc343cb34ad
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-01-26 16:04:00 +0200

    Part 1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,88 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region Part 1
+
+
+            //struct Point
+            //        {
+            //            public double X { get; set; }
+            //            public double Y { get; set; }
+
+            //            public Point(double x, double y)
+            //            {
+            //                X = x;
+            //                Y = y;
+            //            }
+
+            //            public double DistanceTo(Point other)
+            //            {
+            //                return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+            //            }
+            //        }
+
+            //        static void Part1()
+            //        {
+            //            Console.WriteLine("Enter the coordinates of the first point (X Y):");
+            //            string[] input1 = Console.ReadLine().Split();
+            //            Point p1 = new Point(double.Parse(input1[0]), double.Parse(input1[1]));
+
+            //            Console.WriteLine("Enter the coordinates of the second point (X Y):");
+            //            string[] input2 = Console.ReadLine().Split();
+            //            Point p2 = new Point(double.Parse(input2[0]), double.Parse(input2[1]));
+
+            //            double distance = p1.DistanceTo(p2);
+            //            Console.WriteLine($"The distance between the two points is: {distance:F2}");
+            //        }
+
+
+
+
+            //        struct Person
+            //        {
+            //            public string Name { get; set; }
+            //            public int Age { get; set; }
+
+            //            public Person(string name, int age)
+            //            {
+            //                Name = name;
+            //                Age = age;
+            //            }
+            //        }
+
+            //        static void Part1_OldestPerson()
+            //        {
+            //            Person[] persons = new Person[3];
+
+            //            for (int i = 0; i < 3; i++)
+            //            {
+            //                Console.WriteLine($"Enter the name of person {i + 1}:");
+            //                string name = Console.ReadLine();
+
+            //                Console.WriteLine($"Enter the age of person {i + 1}:");
+            //                int age = int.Parse(Console.ReadLine());
+
+            //                persons[i] = new Person(name, age);
+            //            }
+
+            //            Person oldest = persons[0];
+            //            for (int i = 1; i < persons.Length; i++)
+            //            {
+            //                if (persons[i].Age > oldest.Age)
+            //                {
+            //                    oldest = persons[i];
+            //                }
+            //            }
+
+            //            Console.WriteLine($"The oldest person is {oldest.Name} with age {oldest.Age}.");
+            //        }
+
+
+            #endregion
+
+
+          
+
     }
+}
 }
